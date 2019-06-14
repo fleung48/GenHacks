fc2d9c42a25bfeb43e80a86b3968e7a434ef27c3
e-zhang09 <36081769+e-zhang09@users.noreply.github.com>
2019-06-14 14:06:22 -0700

    Fixed Schedule table for both web & mobile

--- public/javascript/content.js
+++ public/javascript/content.js
@@ -1,11 +1,11 @@
 $(".container-arrow").click(function(){
    $([document.documentElement, document.body]).animate({
       scrollTop: $("#content-start").offset().top
-   }, 2000);
+   }, 1500);
 });
 
 function onLoad () {
-   $(".headline").delay(250).animate({"opacity": "1"}, 1750);
+   $(".headline").delay(250).animate({"opacity": "1"}, 1550);
 
    $(function(){
       $("#event-schedule-container").load("/views/event-schedule.html");
