5b1231a5bb4d20b2f4c73e9177991a123d9bbee4
e-zhang09 <36081769+e-zhang09@users.noreply.github.com>
2019-06-14 04:40:48 -0700

    Mobile Schedule work... ish

--- public/javascript/content.js
+++ public/javascript/content.js
@@ -3,3 +3,22 @@ $(".container-arrow").click(function(){
       scrollTop: $("#content-start").offset().top
    }, 2000);
 });
+
+function onLoad () {
+   $(".headline").delay(250).animate({"opacity": "1"}, 1750);
+
+   $(function(){
+      $("#event-schedule-container").load("/views/event-schedule.html");
+   });
+   $(function(){
+      $("#faq-container").load("/views/faq.html");
+   });
+   $(function(){
+      $("#sponsors-container").load("/views/sponsors.html");
+   });
+   $(function(){
+      $("#socialmedia-container").load("/views/media-bar.html");
+   });
+}
+
+onLoad();
