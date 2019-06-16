b85fcf8825d2789a1a65e41f6468f1e43c5e9a2c
e-zhang09 <36081769+e-zhang09@users.noreply.github.com>
2019-06-16 00:30:11 -0700

    About to change over to solid color, fixed mobile photos

--- public/javascript/content.js
+++ public/javascript/content.js
@@ -9,3 +9,13 @@ function onLoad () {
 }
 
 onLoad();
+
+var src = $('#test').css('background-image');
+var url = src.match(/\((.*?)\)/)[1].replace(/('|")/g,'');
+
+var img = new Image();
+img.onload = function() {
+   alert('image loaded');
+};
+img.src = url;
+if (img.complete) img.onload();
