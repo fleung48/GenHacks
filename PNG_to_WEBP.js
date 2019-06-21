d0780e0b41006f95d24651fdc23f02d9c272c20a
e-zhang09 <36081769+e-zhang09@users.noreply.github.com>
2019-06-20 23:32:33 -0700

    Trying to switch over to webp

--- PNG_to_WEBP.js
+++ PNG_to_WEBP.js
@@ -1,8 +1,8 @@
 let imagemin = require("imagemin"),    // The imagemin module.
     webp = require("imagemin-webp"),   // imagemin's WebP plugin.
-    outputFolder = "./img",            // Output folder
-    PNGImages = "./img/*.png",         // PNG images
-    JPEGImages = "./img/*.jpg";        // JPEG images
+    outputFolder = "./public/res/portraits",            // Output folder
+    PNGImages = "./public/res/portraits/*.png",         // PNG images
+    JPEGImages = "./public/res/portraits/*.jpg";        // JPEG images
 
 imagemin([PNGImages], outputFolder, {
     plugins: [webp({
@@ -12,6 +12,6 @@ imagemin([PNGImages], outputFolder, {
 
 imagemin([JPEGImages], outputFolder, {
     plugins: [webp({
-        quality: 65 // Quality setting from 0 to 100
+        quality: 80 // Quality setting from 0 to 100
     })]
 });
